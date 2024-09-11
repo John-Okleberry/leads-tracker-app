4b123808d477e5793318645bf0bf701049a91bca
John Okleberry <John-Okleberry@users.noreply.github.com>
2024-09-10 20:58:50 -0700

    Attempt to update database URL for Netlify

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const deleteBtn = document.getElementById("delete-btn")
 
 // Provides the Firebase database URL (works through dotenv)
 const firebaseConfig = {
-	databaseURL: process.env.DATABASE_URL
+	// databaseURL: process.env.DATABASE_URL
+    databaseURL: import.meta.env.VITE_DATABASE_URL
   }
 
 const app = initializeApp(firebaseConfig)
