674fe7a415092e800112607805aa87a40573e1b5
John Okleberry <John-Okleberry@users.noreply.github.com>
2024-09-10 17:04:29 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Imports from Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js"
+import { getDatabase,
+         ref,
+         push,
+         onValue,
+         remove } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js"  
+
+// Elements on the page for interaction
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+//
+// Variables for connection to the database in firebase
+//
+
+// Provides the Firebase database URL (works through dotenv)
+const firebaseConfig = {
+	databaseURL: process.env.DATABASE_URL
+  }
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
+
+// Displays the array of leads to the user
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+// When a change is made to the database creates an array from the DB object values and renders it
+onValue(referenceInDB, function(snapshot) {
+    const snapshotDoesExist = snapshot.exists()
+    if(snapshotDoesExist){
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+})
+
+// Removes the current data from the database and removes the old data from the UI
+deleteBtn.addEventListener("dblclick", function() {
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
+})
+
+// Pushes the input value to the database and clears the input value
+inputBtn.addEventListener("click", function() {
+    push(referenceInDB, inputEl.value)
+    inputEl.value = ""
+})
